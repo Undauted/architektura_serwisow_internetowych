3498b995d900db8968b27d267d57b7c264f95902
Piotr Kacprowicz <piecia2468@gmail.com>
2015-04-10 21:38:38 +0000

    Add account activations

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "piecia2468@gmail.com"
+  layout 'mailer'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "undauted",
              email: "piotrkacprowicz92@gmail.com",
              password:              "lysy10",
              password_confirmation: "lysy10",
-               admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
              
 
 99.times do |n|
@@ -12,5 +14,7 @@ User.create!(name:  "undauted",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+             password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end

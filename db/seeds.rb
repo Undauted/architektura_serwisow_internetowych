e898df65788e894dca403c4ba6213b348ca54d9d
Piotr Kacprowicz <piecia2468@gmail.com>
2015-04-11 19:09:10 +0000

    ad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@ User.create!(name:  "undauted",
              admin:     true)
              
 
-99.times do |n|
+50.times do |n|
   name  = Faker::Name.name
-  email = "piotrkacprowicz92-#{n+1}@gmail.com"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
-             password_confirmation: password)
+               password_confirmation: password)
 end
 
 users = User.order(:created_at).take(6)
-50.times do
+20.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
@@ -24,7 +24,7 @@ end
 # Following relationships
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..30]
+followers = users[3..20]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
